601e6a7b952fb8f48a65ae1463db5f8538c36450
DonShanilka <shanilka799.com>
2024-11-21 05:14:07 -0800

    Initial commit

---
+++ src/main/java/lk/ijse/controller/DashBordController.java
@@ -0,0 +1,53 @@
+package lk.ijse.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+import java.awt.*;
+
+public class DashBordController {
+
+    @FXML
+    private Label lbldesc;
+
+    @FXML
+    private Label lblmenu;
+
+    @FXML
+    private ImageView course;
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private ImageView settings;
+
+    @FXML
+    private ImageView student;
+
+    @FXML
+    private ImageView transaction;
+
+    @FXML
+    void logoutOnAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void navigate(MouseEvent event) {
+
+    }
+
+    @FXML
+    void playMouseEnterAnimation(MouseEvent event) {
+
+    }
+
+    @FXML
+    void playMouseExitAnimation(MouseEvent event) {
+
+    }
+
+}
